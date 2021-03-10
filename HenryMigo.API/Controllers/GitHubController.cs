1eeaf7b47c889c43812ad8e6c5f1901f55089173
Henry Migo <henryeamigo@gmail.com>
2021-03-10 22:44:07 +0000

    Styled out the repo's page

--- HenryMigo.API/Controllers/GitHubController.cs
+++ HenryMigo.API/Controllers/GitHubController.cs
@@ -21,7 +21,7 @@ namespace HenryMigo.API.Controllers
         public async Task<IReadOnlyList<Repository>> Get()
         {
             var client = new GitHubClient(new ProductHeaderValue("HenryMigo.dev"));
-            var auth = new Credentials("e8dcb5236e8bf6c5de38d5365f64daecea8b65cd");
+            var auth = new Credentials("f8eb5e5cd7861837b4f7bf08d84245841216a6fe");
             client.Credentials = auth;
 
             var repos = await client.Repository.GetAllForUser("HenryMigo");
